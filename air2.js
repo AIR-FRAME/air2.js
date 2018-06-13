f41bf84bbcd531a7df847035bd2a57c6a89f49c5
Bobby Thomas <30844492+AIR-FRAME@users.noreply.github.com>
2018-06-13 15:27:25 -0700

    Update air2.js

--- air2.js
+++ air2.js
@@ -2,267 +2,6 @@
 
 var Air2 = function(){};
 
-
-window.onload = function(){	
-	_prepareFormPost();
-	_generateDashboard();
-}
-
-window.onerror = function(err, page, line){
-	alert("E R R O R \n\nline: " + line + "\n\n" + err + "\n\n" + page);
-}
-
-
-function redirect(url){
-	var dx = document.location.href.substr(0, document.location.href.indexOf("/app/"));
-	document.location.href = dx + url;																			/*COMMON FUCNTIONS*/
-}
-
-function __remove(row, _this){	
-	if(_this.innerHTML == "Delete"){
-		row.setAttribute("deleted", "1");
-		for(var rc=0;rc<row.cells.length-1;rc++){
-			row.cells[rc].style.textDecoration = "line-through";
-		}
-		row.cells[row.cells.length-1].innerHTML = "Undo Delete";
-	
-		if(row.nextSibling.cells && row.nextSibling.cells.length == 1){
-			row.nextSibling.style.display = "none";	
-		}
-	}
-	else{
-		row.setAttribute("deleted", "");
-		for(var rc=0;rc<row.cells.length-1;rc++){
-			row.cells[rc].style.textDecoration = "";
-		}
-		row.cells[row.cells.length-1].innerHTML = "Delete";		
-	}
-}
-
-function _get(gc, path, CALLBACK){
-	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function() {
-		if (xhttp.readyState == 4 && xhttp.status == 200) {
-			CALLBACK(gc, xhttp.responseText);																		/*COMMON FUCNTIONS*/
-		}
-	};
-	xhttp.open("GET", path, true);
-	xhttp.send();
-}
-
-function _post(gc, path, data, CALLBACK, ERRORCALLBACK){
-	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function() {
-		if (xhttp.readyState == 4) {
-			if(xhttp.status == 200){
-				CALLBACK(gc, xhttp.responseText);
-			}
-			else{
-				ERRORCALLBACK(gc, "ERROR : " + xhttp.status + "\n\n" +  xhttp.responseText);
-			}
-		}
-	};
-	xhttp.open("POST", path, true);
-	xhttp.setRequestHeader("Content-Type", "application/json")
-	xhttp.send(data);
-}
-
-function _connectToEditForm(row, form){
-	var elem = form.querySelectorAll("input,select,textarea"); 
-	for(var c=0;c<elem.length;c++){																					/*COMMON FUCNTIONS*/
-		var cBind = elem[c].getAttribute("bind");		
-		if(cBind){
-			for(var rc=0;rc<row.cells.length;rc++){
-				var rcBind = row.parentNode.parentNode.rows[0].cells[rc].getAttribute("bind");
-				if(rcBind == cBind){					
-					elem[c].value = row.cells[rc].innerText;
-					break;
-				}
-			}
-		}
-	}
-}
-
-function _fillLists(form, CALLBACK){
-	var callCount = 0;
-	var elem = form.querySelectorAll("select"); 
-	for(var c=0;c<elem.length;c++){
-		var cList = elem[c].getAttribute("list");
-		if(cList){
-			callCount++;
-			_get(elem[c], "../data/"+cList+".json", function(obj, DATA){	
-				callCount--;
-				var list = eval("["+DATA+"]");
-				var options = "";
-				for(var r=0;r<list.length;r++){
-					options += "<option value=\""+list[r]["value"]+"\">"+list[r]["text"]+"</option>";
-				}																									/*COMMON FUCNTIONS*/
-				obj.innerHTML =  options;
-				if(callCount <= 0){
-					CALLBACK();				
-				}
-			});	
-		}
-	}
-	if(callCount <= 0){
-		CALLBACK();				
-	}
-}
-
-function getParameterByName(name, url) {
-	if (!url) url = window.location.href;
-	name = name.replace(/[\[\]]/g, "\\$&");
-	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-		results = regex.exec(url);
-	if (!results) return null;
-	if (!results[2]) return '';
-	return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
-
-function __edit(index, row, _this){	
-	if(row.getAttribute("deleted")){
-		alert("This record is marked for deletion.");
-		return;
-	}
-	
-	var _editTemplate = row.parentNode.parentNode.parentNode.getAttribute("edit-template");
-	var _editForm = row.parentNode.parentNode.parentNode.getAttribute("edit-form");
-	var _editAction = row.parentNode.parentNode.parentNode.getAttribute("edit-action");
-	if(_editForm){
-		if(_editAction && _editAction === "openRecordInTab"){														/*COMMON FUCNTIONS*/
-			FORM.openRecordInTab(index, _this.parentNode, _this, _editForm);
-		}
-		else{
-			alert("Action not registered.");
-		}
-	}
-	else if(_editTemplate){
-		if(row.nextSibling.getAttribute && row.nextSibling.getAttribute("editFrame")){
-			row.nextSibling.style.display = row.nextSibling.style.display ? "":"none";			
-		}
-		else{		
-			if(_editTemplate){
-				_get(null, _editTemplate, function(obj, DATA){	
-					var newRow = row.cloneNode(true);
-					var cLength = newRow.cells.length;
-					for(var c=newRow.cells.length-1;c>0;c--){
-						newRow.deleteCell(c);
-					}
-					newRow.setAttribute("editFrame", "true");
-					newRow.cells[0].style.height = "140px";
-					newRow.cells[0].innerHTML = DATA;
-					newRow.cells[0].colSpan = cLength;	
-					newRow.style.backgroundColor = "#bfb";					
-					row.parentNode.insertBefore(newRow, row.nextSibling);
-					_fillLists(newRow.cells[0].firstChild, function(){//CALLBACK
-						_connectToEditForm(row, newRow.cells[0].firstChild);
-					});					
-				});
-			}
-			else{
-				alert("No edit template specified for this grid.");
-			}																										/*COMMON FUCNTIONS*/
-		}
-	}
-}
-
-function _generateDashboard(){
-	if(typeof CHART != "undefined"){
-		setTimeout(function(){																				/*COMMON FUCNTIONS*/
-			var piechart1 = document.getElementById('piechart1');
-			var data1 = [200, 60, 15];
-			var color1 = ['#F17CB0', '#4D4D4D', '#5DA5DA'];
-			CHART.PieChart(piechart1, data1, color1);
-			
-			var piechart2 = document.getElementById('piechart2');
-			var data2 = [80, 115, 55, 25];
-			var color2 = ['#B276B2', '#4D4D4D', '#DECF3F', '#60BD68'];
-			CHART.PieChart(piechart2, data2, color2);
-			
-			var gradient1 = document.getElementById('gradient1');			
-			//CHART.Text(gradient1, 'hello mam', 10, 50);			
-			CHART.barGraph(gradient1);	
-			
-		}, 100);
-	}
-}
-
-function _prepareFormPost(){
-	var _submit = document.getElementById("submit");
-	if(_submit){		
-		_submit.onclick = function(){
-			var posturl = this.getAttribute("posturl");
-			if(!posturl){
-				alert("Action not defined.");
-				return;
-			}
-		
-			var params = "";
-			
-			for(var pc=0;pc<FORM.CONTROLS.length;pc++){
-				params += "\"FORM" + pc + "\":[{";
-				for(var c=0;c<FORM.CONTROLS[pc].length;c++){
-					var Control = FORM.CONTROLS[pc][c].Control;
-					if(Control){
-						var Bind = FORM.CONTROLS[pc][c].Control.getAttribute("bind");
-						if(Bind){
-							params += "\"" + Bind + "\":\"" + Control.value + "\", ";
-						}
-					}																							/*COMMON FUCNTIONS*/
-				}
-				if(params){
-					params = params.substr(0, params.length-2);
-				}
-				params += "}],";
-			}
-			
-			//Access Grids
-			var alltables = document.getElementsByTagName("TABLE");
-			for(var t=0;t<alltables.length;t++){
-				var table = alltables[t];
-				if(table.getAttribute("modified")){
-					params += "\"GRID" + t + "\":[{";
-					for(var r=1;r<table.rows.length;r++){
-						var rowModified = table.rows[r].getAttribute("modified");
-						if(rowModified && rowModified == "1"){
-							for(var c=0;c<table.rows[r].cells.length;c++){
-								var cellModified = table.rows[r].cells[c].getAttribute("modified");
-								if(cellModified && cellModified == "1"){
-									var Bind = table.rows[0].cells[c].getAttribute("bind");
-									var Value = table.rows[r].cells[c].innerHTML;
-									//alert(Bind + " --> " + Value);
-									params += "\"" + Bind + "\":\"" + Value + "\", ";
-								}
-							}
-						}
-					}
-					params += "}],";
-				}																								/*COMMON FUCNTIONS*/
-			}
-			
-			
-			if(params){
-				params = params.substr(0, params.length-1);
-			}
-			//params = encodeURIComponent(params);
-			alert(params)
-			params = "{\"ID\":\"10000001\", \"FIRSTNAME\":\"BOB\", \"LASTNAME\":\"THO\", \"COMPANY\":\"HID\"}";
-			
-			//alert("params: " + params);
-			_post(1, posturl, params, function(obj, DATA){
-				alert(DATA);
-			}, function(obj, ERROR){
-				alert(ERROR);
-			});
-		}
-	}
-}
-
-//***********************************************************************************************************************************************
-//***********************************************************************************************************************************************
-//***********************************************************************************************************************************************
-
-
 var FORM = function (_def) {
 	var container;																									/*FORM OBJECT*/
 	var _containerClass = "container";
@@ -758,6 +497,257 @@ NAV.render();																												/*INVOKING*/
 FORM.render(function(){
 	GRID.render();
 });
+
+
+function redirect(url){
+	var dx = document.location.href.substr(0, document.location.href.indexOf("/app/"));
+	document.location.href = dx + url;																			/*COMMON FUCNTIONS*/
+}
+
+function __remove(row, _this){	
+	if(_this.innerHTML == "Delete"){
+		row.setAttribute("deleted", "1");
+		for(var rc=0;rc<row.cells.length-1;rc++){
+			row.cells[rc].style.textDecoration = "line-through";
+		}
+		row.cells[row.cells.length-1].innerHTML = "Undo Delete";
+	
+		if(row.nextSibling.cells && row.nextSibling.cells.length == 1){
+			row.nextSibling.style.display = "none";	
+		}
+	}
+	else{
+		row.setAttribute("deleted", "");
+		for(var rc=0;rc<row.cells.length-1;rc++){
+			row.cells[rc].style.textDecoration = "";
+		}
+		row.cells[row.cells.length-1].innerHTML = "Delete";		
+	}
+}
+
+function _get(gc, path, CALLBACK){
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (xhttp.readyState == 4 && xhttp.status == 200) {
+			CALLBACK(gc, xhttp.responseText);																		/*COMMON FUCNTIONS*/
+		}
+	};
+	xhttp.open("GET", path, true);
+	xhttp.send();
+}
+
+function _post(gc, path, data, CALLBACK, ERRORCALLBACK){
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (xhttp.readyState == 4) {
+			if(xhttp.status == 200){
+				CALLBACK(gc, xhttp.responseText);
+			}
+			else{
+				ERRORCALLBACK(gc, "ERROR : " + xhttp.status + "\n\n" +  xhttp.responseText);
+			}
+		}
+	};
+	xhttp.open("POST", path, true);
+	xhttp.setRequestHeader("Content-Type", "application/json")
+	xhttp.send(data);
+}
+
+function _connectToEditForm(row, form){
+	var elem = form.querySelectorAll("input,select,textarea"); 
+	for(var c=0;c<elem.length;c++){																					/*COMMON FUCNTIONS*/
+		var cBind = elem[c].getAttribute("bind");		
+		if(cBind){
+			for(var rc=0;rc<row.cells.length;rc++){
+				var rcBind = row.parentNode.parentNode.rows[0].cells[rc].getAttribute("bind");
+				if(rcBind == cBind){					
+					elem[c].value = row.cells[rc].innerText;
+					break;
+				}
+			}
+		}
+	}
+}
+
+function _fillLists(form, CALLBACK){
+	var callCount = 0;
+	var elem = form.querySelectorAll("select"); 
+	for(var c=0;c<elem.length;c++){
+		var cList = elem[c].getAttribute("list");
+		if(cList){
+			callCount++;
+			_get(elem[c], "../data/"+cList+".json", function(obj, DATA){	
+				callCount--;
+				var list = eval("["+DATA+"]");
+				var options = "";
+				for(var r=0;r<list.length;r++){
+					options += "<option value=\""+list[r]["value"]+"\">"+list[r]["text"]+"</option>";
+				}																									/*COMMON FUCNTIONS*/
+				obj.innerHTML =  options;
+				if(callCount <= 0){
+					CALLBACK();				
+				}
+			});	
+		}
+	}
+	if(callCount <= 0){
+		CALLBACK();				
+	}
+}
+
+function getParameterByName(name, url) {
+	if (!url) url = window.location.href;
+	name = name.replace(/[\[\]]/g, "\\$&");
+	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+		results = regex.exec(url);
+	if (!results) return null;
+	if (!results[2]) return '';
+	return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function __edit(index, row, _this){	
+	if(row.getAttribute("deleted")){
+		alert("This record is marked for deletion.");
+		return;
+	}
+	
+	var _editTemplate = row.parentNode.parentNode.parentNode.getAttribute("edit-template");
+	var _editForm = row.parentNode.parentNode.parentNode.getAttribute("edit-form");
+	var _editAction = row.parentNode.parentNode.parentNode.getAttribute("edit-action");
+	if(_editForm){
+		if(_editAction && _editAction === "openRecordInTab"){														/*COMMON FUCNTIONS*/
+			FORM.openRecordInTab(index, _this.parentNode, _this, _editForm);
+		}
+		else{
+			alert("Action not registered.");
+		}
+	}
+	else if(_editTemplate){
+		if(row.nextSibling.getAttribute && row.nextSibling.getAttribute("editFrame")){
+			row.nextSibling.style.display = row.nextSibling.style.display ? "":"none";			
+		}
+		else{		
+			if(_editTemplate){
+				_get(null, _editTemplate, function(obj, DATA){	
+					var newRow = row.cloneNode(true);
+					var cLength = newRow.cells.length;
+					for(var c=newRow.cells.length-1;c>0;c--){
+						newRow.deleteCell(c);
+					}
+					newRow.setAttribute("editFrame", "true");
+					newRow.cells[0].style.height = "140px";
+					newRow.cells[0].innerHTML = DATA;
+					newRow.cells[0].colSpan = cLength;	
+					newRow.style.backgroundColor = "#bfb";					
+					row.parentNode.insertBefore(newRow, row.nextSibling);
+					_fillLists(newRow.cells[0].firstChild, function(){//CALLBACK
+						_connectToEditForm(row, newRow.cells[0].firstChild);
+					});					
+				});
+			}
+			else{
+				alert("No edit template specified for this grid.");
+			}																										/*COMMON FUCNTIONS*/
+		}
+	}
+}
+
+function _generateDashboard(){
+	if(typeof CHART != "undefined"){
+		setTimeout(function(){																				/*COMMON FUCNTIONS*/
+			var piechart1 = document.getElementById('piechart1');
+			var data1 = [200, 60, 15];
+			var color1 = ['#F17CB0', '#4D4D4D', '#5DA5DA'];
+			CHART.PieChart(piechart1, data1, color1);
+			
+			var piechart2 = document.getElementById('piechart2');
+			var data2 = [80, 115, 55, 25];
+			var color2 = ['#B276B2', '#4D4D4D', '#DECF3F', '#60BD68'];
+			CHART.PieChart(piechart2, data2, color2);
+			
+			var gradient1 = document.getElementById('gradient1');			
+			//CHART.Text(gradient1, 'hello mam', 10, 50);			
+			CHART.barGraph(gradient1);	
+			
+		}, 100);
+	}
+}
+
+function _prepareFormPost(){
+	var _submit = document.getElementById("submit");
+	if(_submit){		
+		_submit.onclick = function(){
+			var posturl = this.getAttribute("posturl");
+			if(!posturl){
+				alert("Action not defined.");
+				return;
+			}
+		
+			var params = "";
+			
+			for(var pc=0;pc<FORM.CONTROLS.length;pc++){
+				params += "\"FORM" + pc + "\":[{";
+				for(var c=0;c<FORM.CONTROLS[pc].length;c++){
+					var Control = FORM.CONTROLS[pc][c].Control;
+					if(Control){
+						var Bind = FORM.CONTROLS[pc][c].Control.getAttribute("bind");
+						if(Bind){
+							params += "\"" + Bind + "\":\"" + Control.value + "\", ";
+						}
+					}																							/*COMMON FUCNTIONS*/
+				}
+				if(params){
+					params = params.substr(0, params.length-2);
+				}
+				params += "}],";
+			}
+			
+			//Access Grids
+			var alltables = document.getElementsByTagName("TABLE");
+			for(var t=0;t<alltables.length;t++){
+				var table = alltables[t];
+				if(table.getAttribute("modified")){
+					params += "\"GRID" + t + "\":[{";
+					for(var r=1;r<table.rows.length;r++){
+						var rowModified = table.rows[r].getAttribute("modified");
+						if(rowModified && rowModified == "1"){
+							for(var c=0;c<table.rows[r].cells.length;c++){
+								var cellModified = table.rows[r].cells[c].getAttribute("modified");
+								if(cellModified && cellModified == "1"){
+									var Bind = table.rows[0].cells[c].getAttribute("bind");
+									var Value = table.rows[r].cells[c].innerHTML;
+									//alert(Bind + " --> " + Value);
+									params += "\"" + Bind + "\":\"" + Value + "\", ";
+								}
+							}
+						}
+					}
+					params += "}],";
+				}																								/*COMMON FUCNTIONS*/
+			}
+			
+			
+			if(params){
+				params = params.substr(0, params.length-1);
+			}
+			//params = encodeURIComponent(params);
+			alert(params)
+			params = "{\"ID\":\"10000001\", \"FIRSTNAME\":\"BOB\", \"LASTNAME\":\"THO\", \"COMPANY\":\"HID\"}";
+			
+			//alert("params: " + params);
+			_post(1, posturl, params, function(obj, DATA){
+				alert(DATA);
+			}, function(obj, ERROR){
+				alert(ERROR);
+			});
+		}
+	}
+}
+
+//***********************************************************************************************************************************************
+//***********************************************************************************************************************************************
+//***********************************************************************************************************************************************
+
 
 if( typeof module !== "undefined" && ('exports' in module)){
    module.exports = Air2;
